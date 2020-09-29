414f66daaeae2df00e178afcc70a7e45112d40a8
mks-94 <muralikrishna9424@gmail.com>
2020-09-29 21:41:20 +0530

    ready for prod

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://place_user_db:user123@cluster0.szkhn.mongodb.net/mern?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.szkhn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
